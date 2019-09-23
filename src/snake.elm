9eefdcfa05f59738ca0ec83470e77de137052b50
Andrew Cline <andrew.cline77@gmail.com>
2019-09-22 20:37:25 -0500

    add moving box in game

--- src/snake.elm
+++ src/snake.elm
@@ -4,4 +4,14 @@ import Playground exposing (..)
 
 
 main =
-    game
+    game view update 0
+
+
+view computer offset =
+    [ square blue 30
+        |> moveRight offset
+    ]
+
+
+update computer offset =
+    offset + 1
